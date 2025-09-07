6f846b5359fa3e0f783c31b3a9a4130769a4e065
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2025-09-07 22:35:14 +0530

    add bew pages

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
-export const page = () => {
+export default function Signin() {
   return (
-    <div>page</div>
+    <main className="sign-in">
+      <aside className="testimonial">
+        <Link href="/" className="flex text-center gap-2">
+          <Image src={"/assets/icons/logo.svg"} alt="logo" width={32} height={32} />
+          <h1>ManRcder</h1>
+        </Link>
+        <div className="description">
+          <section>
+            <figure>
+              {
+                Array.from({ length: 5 }).map((_, index) => (
+                  <Image src={"/assets/icons/star.svg"} alt="star" width={32} height={32} key={index} />
+                ))
+              }
+            </figure>
+            <p>
+              SnapCast makes screen recording easy. From quick walkthroughs to full presentations, it's fast, smooth, and shareable in seconds
+            </p>
+            <article>
+              <Image src={"/assets/images/jason.png"} alt="logo" width={64} height={64} className="rounded-full" />
+              <div>
+                <h2>
+                  Andrian jason
+                </h2>
+                <p>product Designer ,Open Ai</p>
+              </div>
+            </article>
+          </section>
+        </div>
+        <p className="text-center">
+          @ ManRcder {(new Date().getFullYear())}
+        </p>
+      </aside>
+      <aside className="google-sign-in">
+        <section>
+          <Link href={"/"}>
+            <Image src={"/assets/icons/logo.svg"} alt={"logo"} width={40} height={40} />
+            <h1>
+              ManRcder
+            </h1>
+          </Link>
+          <p>Create and share your very first video <span>ManRcder</span> in Free mode and enjoy Recording with loved ones</p>
+          <button>
+            <Image src={"/assets/icons/google.svg"} alt="logo"  width={34} height={34}/>
+            <span>Sign-in with Google</span>
+          </button>
+        </section>
+      </aside>
+    </main>
   )
 }
